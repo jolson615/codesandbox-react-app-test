841115293d569e654f2b0e9a33c184f4b3c4ecbb
Jeff <jolson615@gmail.com>
2018-12-21 12:33:05 -0500

    Adds poorly styled navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="nav-item">Home</div>
+      <div className="nav-item">About</div>
+      <div className="nav-item">Contact Us</div>
+      <div className="nav-item">Apply Please</div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Navbar from "./Navbar/Navbar";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <Navbar />
+      <h1>Hello This is a serious test</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
